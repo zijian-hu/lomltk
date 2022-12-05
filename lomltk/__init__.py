fc0e6e6012743e3e05cd38e044ab4c841186d0da
Zijian Hu <zijianhu@usc.edu>
2022-12-05 15:21:12 -0800

    update (sub)module imports

--- lomltk/__init__.py
+++ lomltk/__init__.py
@@ -1,3 +1,8 @@
 from .utils import *
 
+# submodules
+from . import algorithm
+from . import pandas
+from . import torch
+
 __version__ = "0.0.1.post1"
--- lomltk/torch/__init__.py
+++ lomltk/torch/__init__.py
@@ -1,1 +1,6 @@
 from .utils import *
+
+# submodules
+from . import data
+from . import distributed
+from . import models
